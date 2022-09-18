62fff7883046b9822f181584c590dc446722d714
Enes Usta <enesusta@protonmail.com>
2022-09-18 16:41:47 +0300

    BSB-5 feat: crud book ops

--- src/main/java/com/bibliyomani/standalone/bff/repository/BookMetadataRepository.java
+++ src/main/java/com/bibliyomani/standalone/bff/repository/BookMetadataRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookMetadataRepository extends JpaRepository<BookMetadata, Integer> {
+    BookMetadata findByHash(String hash);
 }
